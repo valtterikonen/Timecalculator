f0265be59f95cc7496d7dba81f2a988cf4311b7e
valtterikonen <122282094+valtterikonen@users.noreply.github.com>
2024-09-20 15:32:26 +0300

    Delete GameState directory

--- GameState/src/State.java
+++
@@ -1,6 +0,0 @@
-public interface State {
-    void train(GameCharacter character);
-    void meditate(GameCharacter character);
-    void fight(GameCharacter character);
-    void displayStatus(GameCharacter character);
-}
